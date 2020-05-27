4f09957602e7d6e6057e580f807c618a44e5a068
richard <13160658846@163.com>
2020-05-27 23:50:19 +0800

    _0704_binary_search

--- src/main/java/binarysearch/_0704_binary_search.java
+++ src/main/java/binarysearch/_0704_binary_search.java
@@ -2,11 +2,38 @@ package binarysearch;
 
 /**
  * @author Richard.Zhan
- * @Description: 寻找一个数
+ * @Description: binary search
  * 基本的二分搜索
+ *
+ * Given a sorted (in ascending order) integer array nums of n elements and a target value, write a function to search
+ * target in nums. If target exists, then return its index, otherwise return -1.
+ *
+ *
+ * Example 1:
+ *
+ * Input: nums = [-1,0,3,5,9,12], target = 9
+ * Output: 4
+ * Explanation: 9 exists in nums and its index is 4
+ *
+ * Example 2:
+ *
+ * Input: nums = [-1,0,3,5,9,12], target = 2
+ * Output: -1
+ * Explanation: 2 does not exist in nums so return -1
+ *  
+ *
+ * Note:
+ *
+ * You may assume that all elements in nums are unique.
+ * n will be in the range [1, 10000].
+ * The value of each element in nums will be in the range [-9999, 9999].
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/binary-search
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  * @date 2020/5/26 13:52
  */
-public class _base_binary_search {
+public class _0704_binary_search {
 
 
     /**
@@ -30,8 +57,12 @@ public class _base_binary_search {
             if (nums[mid] == target) {
                 return mid;
             } else if (nums[mid] < target){
+                // 有数组的有序性可知，mid和mid的左边都小于等于 target
+                // 下一轮搜索的范围为 [left + 1, right]
                 left = mid + 1;
             } else if (nums[mid] > target){
+                // min和mid的右边都小于等于 target
+                // 下一轮搜索的范围为 [left , right + 1]
                 right = mid - 1;
             }
         }
@@ -167,7 +198,7 @@ public class _base_binary_search {
 
 
     public static void main(String[] args) {
-        _base_binary_search instance = new _base_binary_search();
+        _0704_binary_search instance = new _0704_binary_search();
         int[] nums = new int[]{1,2,3,4,5,6};
         int target = 5;
         // 基本二分搜索
